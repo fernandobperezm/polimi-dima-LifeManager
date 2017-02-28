6e50a97ff4db5b5f0a0d7920008410f089208e56
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2017-02-28 21:56:09 +0100

    Changes.

--- LifeManager/app/src/main/java/fernandoperez/lifemanager/spotifyapi/fragments/SpotifyPlaybackFragment.java
+++ LifeManager/app/src/main/java/fernandoperez/lifemanager/spotifyapi/fragments/SpotifyPlaybackFragment.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
