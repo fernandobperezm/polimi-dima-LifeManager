4db1466747ea25644576fba550b2b249785a8e2e
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2017-03-02 01:42:58 +0100

    Refactored constants.java code.

--- LifeManager/app/src/main/java/fernandoperez/lifemanager/utils/constants.java
+++ LifeManager/app/src/main/java/fernandoperez/lifemanager/utils/constants.java
@@ -1,20 +1,11 @@
 package fernandoperez.lifemanager.utils;
 
-import android.content.res.Configuration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.util.DisplayMetrics;
-
-/**
- * Created by fernandoperez on 1/26/17.
- */
-
 public abstract class constants {
     // INTENT CODES.
     public final static int TWITTER_MAX_HOME_TIMELINE_TWEETS = 50;
     public final static int REQUEST_ENABLE_BT = 69;
 
     public final static String CONFIGURATION_NAME = "CONFIGURATION_NAME";
-    public final static String CONFIGURATION_CURRENT_TYPE = "CONFIGURATION_TYPE";
 
     //
     public final static String BACKUP_FILENAME = "backup.xml";
@@ -36,15 +27,15 @@ public abstract class constants {
         }
     }
 
-    public enum CONFIGURATION_TYPES {
-        ARRIVING(0), LEAVING(1);
-
-        public final int id;
-
-        CONFIGURATION_TYPES(int id) {
-            this.id = id;
-        }
-    }
+//    public enum CONFIGURATION_TYPES {
+//        ARRIVING(0), LEAVING(1);
+//
+//        public final int id;
+//
+//        CONFIGURATION_TYPES(int id) {
+//            this.id = id;
+//        }
+//    }
 
     // Gmail API.
     public final static long MAX_EMAILS_FETCH = 20;
